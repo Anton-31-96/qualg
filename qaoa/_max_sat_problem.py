804b83d04c87ddf4aaf6ad67269215fca4336888
Anton-31-96 <anton@mir-ramok.ru>
2020-03-03 02:24:37 +0300

    calculation of loos of objective function for 3SAT problem

--- qaoa/_max_sat_problem.py
+++ qaoa/_max_sat_problem.py
@@ -3,7 +3,7 @@
 import numpy as np
 
 
-def generator_3sat_new(variable_number, clause_number):
+def generate_3sat(variable_number, clause_number):
     """
     Returns a random 3-SAT problem in CNF form
     (see https://en.wikipedia.org/wiki/Boolean_satisfiability_problem#3-satisfiability)
@@ -29,3 +29,23 @@ def generator_3sat_new(variable_number, clause_number):
             counter = counter + 1
 
     return np.asarray(list_of_clauses)
+
+
+def get_bit(z, i):
+    """
+    gets the i'th bit of the integer z (0 labels least significant bit)
+    """
+    return (z >> i) & 0x1
+
+
+def max_sat_obj(z, clause_list):
+    """
+    Returns loss for a max SAT problem. Here we count the number of violated clauses
+    """
+    loss = 0
+    for var_i, var_j, var_k in clause_list:
+        sign_i, sing_j, sing_k = np.sign(var_i), np.sign(var_j), np.sign(var_k)
+        loss += (1 - sign_i * get_bit(z, var_i)) \
+                * (1 - sing_j * get_bit(z, var_j)) \
+                * (1 - sing_k * get_bit(z, var_k))
+    return loss
