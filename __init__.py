e9eb411e957b717c0350fede8aca071b9bfccf45
Anton-31-96 <anton@mir-ramok.ru>
2020-03-13 17:07:16 +0300

    try import projectq and noise module, it is not necessary for work of dens_mat module

--- __init__.py
+++ __init__.py
@@ -1,12 +1,12 @@
-from qualg import noise
-
-try:
-    from qualg import qaoa
-except ModuleNotFoundError:
-    print('No module ProjectQ')
+# created by Anton Bozhedarov
 
 from ._routines import (generate_3sat,
                         generate_sat_list,
                         get_bit, negation,
                         max_sat_obj
                         )
+try:
+    from qualg import noise
+    from qualg import qaoa
+except ModuleNotFoundError:
+    print('No module ProjectQ')
