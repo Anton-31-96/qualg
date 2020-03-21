978253e89fb8b5bd3d77be598049d564a61249be
Anton-31-96 <anton@mir-ramok.ru>
2020-03-21 03:48:57 +0300

    changes in __init__ file due to new _noise file

--- dens_mat/__init__.py
+++ dens_mat/__init__.py
@@ -1,9 +1,9 @@
 # created by Anton Bozhedarov
 
-from ._qaoa import QAOA_circuit, build_qaoa, generate_data
 from ._noise import (NoiseChannel,
                      DepolChannel,
                      AmplitudePhaseDamping,
                      AmplitudeDamping,
                      PhaseDamping
                      )
+from ._qaoa import QAOA_circuit, build_qaoa, generate_data
